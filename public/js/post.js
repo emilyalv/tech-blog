fceb39e84785f5ad40166f296e0708c2ff878bd8
emilyalv <emilyaalv@gmail.com>
2021-06-09 09:35:39 -0500

    post view page and route

---
+++ public/js/post.js
@@ -0,0 +1,27 @@
+const postFormHandler = async (event) => {
+    event.preventDefault();
+  
+    // Collect values from the login form
+    const title = document.querySelector('#post-title').value.trim();
+    const contents = document.querySelector('#post-contents').value.trim();
+  
+    if (title && contents) {
+      // Send a POST request to the API endpoint
+      const response = await fetch('/api/posts', {
+        method: 'POST',
+        body: JSON.stringify({ title, contents }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        // If successful, redirect the browser to the profile page
+        document.location.replace('/');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };
+
+  document
+  .querySelector('.create-post-form')
+  .addEventListener('submit', postFormHandler);
