b2333095253ae9e1a597fb3d2dadec091e980f1d
emilyalv <emilyaalv@gmail.com>
2021-07-11 11:06:20 -0500

    updated readme

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const session = require('express-session');
 const routes = require('./controllers');
 const db = require('./models');
 const sequelize = require('./config/connection');
+const path = require('path');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const exphbs = require('express-handlebars');
-const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -32,8 +32,6 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
